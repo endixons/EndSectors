bdd886788561779374da35511b0ade97bc28ee1f
Endixon <TEXTR@WP.PL>
2025-12-28 13:26:11 +0100

    update

--- common/src/main/java/pl/endixon/sectors/common/app/AppConfig.java
+++ common/src/main/java/pl/endixon/sectors/common/app/AppConfig.java
@@ -7,11 +7,9 @@ import lombok.Setter;
 public class AppConfig {
     private String natsUrl = "nats://127.0.0.1:4222";
     private String natsClientName = "common-app";
-
     private String redisHost = "127.0.0.1";
     private int redisPort = 6379;
     private String redisPassword = "";
-
     private boolean flowLoggerEnabled = true;
     private long resourceMonitorInterval = 300000;
 }
